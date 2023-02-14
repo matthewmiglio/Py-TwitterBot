4f1ed0406463aaa2d2ad18861052f6e55ea7e40e
Matt Main_PC <matmigg0804@gmail.com>
2023-02-14 13:35:42 -0800

    commenting creds.py

--- login/creds.py
+++ login/creds.py
@@ -4,11 +4,31 @@ import os
 
 # method to get appdata directory
 def get_appdata_directory():
+    """method to get appdata directory
+
+    Args:
+        None
+
+    Returns:
+        string: the appdata directory
+
+    """
+
     return os.getenv("APPDATA")
 
 
 # method to make the creds.txt file at appdata/roaming/Py-TwitterBot\creds.txt
 def make_creds_file():
+    """method to make the creds.txt file at appdata/roaming/Py-TwitterBot\creds.txt
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    """
+
     directory = get_appdata_directory() + r"\py-TwitterBot" + r"\creds.txt"
     with open(directory, "w") as f:
         f.write('Username: ""' + "\n")
@@ -18,12 +38,30 @@ def make_creds_file():
 
 # method to check if creds.txt exists in appdata/roaming/Py-TwitterBot
 def check_if_creds_file_exists():
+    """method to check if creds.txt exists in appdata/roaming/Py-TwitterBot
+
+    Args:
+        None
+
+    Returns:
+        boolean: True if creds.txt exists, False if it does not
+
+    """
+
     directory = get_appdata_directory() + r"\py-TwitterBot" + r"\creds.txt"
     return os.path.exists(directory)
 
 
-# method to read the lines in the creds.txt file
 def get_creds_from_file():
+    """method to get the twitter login credentials stored in creds.txt in the py-twitterbot directory
+
+    Args:
+        None
+
+    Returns:
+        string, string: the username , password
+
+    """
     user = ""
     password = ""
 
@@ -40,6 +78,15 @@ def get_creds_from_file():
 
 
 def parse_cred_line(line):
+    """method to parse the credential line in the creds.txt file
+
+    Args:
+        line: the line to parse
+
+    Returns:
+        string: the parsed line
+
+    """
     new_line = ""
     for char in line:
         if char == " " or char == "\n" or char == '"':
@@ -49,6 +96,15 @@ def parse_cred_line(line):
 
 
 def show_missing_creds_in_auth_file_gui(location):
+    """method to show a gui to the user alerting the mof missing creds in the creds.txt file
+
+    Args:
+        location: the location of the creds.txt file
+
+    Returns:
+        None
+
+    """
     out_text = (
         "TWITTER LOGIN CREDENTIALS MISSING IN creds.txt!\n\n"
         + f"Please write in your Twitter login in the file at [{location}] in the format outlined in the new file.\n"
@@ -68,11 +124,20 @@ def show_missing_creds_in_auth_file_gui(location):
 
 
 def check_if_creds_file_is_empty():
+    """method to check if the creds.txt file is empty
+
+    Args:
+        None
+
+    Returns:
+        boolean: True if the file is empty, False if it is not empty
+
+    """
     creds = get_creds_from_file()
     if creds[0] == "" or creds[1] == "":
         return True
-
-
+    else:
+        return False
 
 
 if not check_if_creds_file_exists():
